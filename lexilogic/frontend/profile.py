6d57c61bd8a2551771b5e18fc58a38c343536eb7
Lestat <2233915@slu.edul.ph>
2024-06-10 23:48:18 +0800

    +profile

---
+++ lexilogic/frontend/profile.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+def run():
+    while(True):
+        os.system('cls||clr')
+        username = os.environ.get("username")
+        print(f"Username: {username}")
+        print("**********************")
+        print("1. Change Username")
+        print("2. Change Password")
+        print("3. Back")
+        print("**********************")
+        choice = input("Choice: ")
+
+def change_password():
+    print()
+
+def change_username():
+    print()
