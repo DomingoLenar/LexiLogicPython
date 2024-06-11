3cbd74a667a410536151c8488a46552376161c06
Lestat <2233915@slu.edul.ph>
2024-06-11 22:45:47 +0800

    +user toleration, authentication skeleten method

--- lexilogic/frontend/login.py
+++ lexilogic/frontend/login.py
@@ -2,10 +2,14 @@ import getpass
 import os
 
 def run():
-    os.system('cls||clr')
-    print("***** Log-In ****")
-    username = input("Username: ")
-    password = getpass.getpass("Password: ")
+    while(True):
+        os.system('cls||clr')
+        print("***** Log-In ****")
+        username = input("Username: ")
+        password = getpass.getpass("Password: ")
+        if(authenticate(username, password)):
+            break
+    
 
     print(f"Username: {username}")
     print(f"Password: [HIDDEN]")
@@ -13,6 +17,10 @@ def run():
     #Insert logic to check if valid
     os.environ['username'] = username
 
+    return True
+
+def authenticate(username: str, password: str):
+    #Use DAL here to authenticate credentials and return boolean
     return True
 
 if __name__ == '__main__':
