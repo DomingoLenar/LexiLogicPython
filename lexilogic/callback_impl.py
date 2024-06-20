af8fc78106b1b89ca20882fc7aa1c7882de292e0
Lestat <2233915@slu.edul.ph>
2024-06-20 22:41:12 +0800

    +Test

---
+++ lexilogic/callback_impl.py
@@ -0,0 +1,29 @@
+import compiled_corba.utility.corbaModules.org__POA
+from lexilogic.update_dispatcher import UpdateDispatcher
+import compiled_corba.utility.corbaModules.org
+
+
+class PlayerCallbackImpl(compiled_corba.utility.corbaModules.org__POA.amalgam.UIControllers.PlayerCallback):
+    username = ""
+    update_dispatch = UpdateDispatcher
+
+    def __init__(self): pass
+
+    def get_username(self):
+        return self.username
+
+    def set_username(self, new_username):
+        self.username = new_username
+
+    def uiCall(self, json_string):
+        self.update_dispatch.update(json_string)
+
+    def controller_interface(self, update_dispatch):
+        self.update_dispatch = update_dispatch
+
+
+if __name__ == "__main__":
+    player_callback_impl = PlayerCallbackImpl()
+    print(dir(player_callback_impl))
+    print(dir(compiled_corba.utility.corbaModules.org.amalgam.UIControllers.PlayerCallback))
+    print(dir(compiled_corba.utility.corbaModules.org__POA.amalgam.UIControllers.PlayerCallback))
---
+++ lexilogic/test.py
@@ -0,0 +1,52 @@
+# Assets shared with members in other packages
+
+import os
+
+os.environ['ORBInitRef'] = 'NameService=corbaloc::corbaserver:2121/NameService'
+import sys
+from omniORB import CORBA
+import CosNaming, PortableServer
+from compiled_corba.utility.corbaModules.org.amalgam import Service  # REQUIRED
+from callback_impl import PlayerCallbackImpl
+
+
+def orb_connection():
+    # Initialize the ORB
+    orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+    return orb
+
+
+def get_nce(orb):
+    root_nce = orb.resolve_initial_references("NameService")
+    child_nce = root_nce._narrow(CosNaming.NamingContextExt)
+    return child_nce
+
+
+def get_player_service_stub(nce):
+    player_service_stub = nce.resolve_str("PlayerService")
+    return player_service_stub
+
+
+def get_game_service_stub(nce):
+    game_service_stub = nce.resolve_str("GameService")
+    return game_service_stub
+
+
+def get_poa(orb):
+    poa_manager = orb.resolve_initial_references("RootPOA")
+    poa = poa_manager._narrow(PortableServer.POA)
+    poa.the_POAManager.activate()
+    return poa
+
+
+if __name__ == "__main__":
+    orb = orb_connection()
+    retrieved_poa = get_poa(orb)
+    nce = get_nce(orb)
+    pss = get_player_service_stub(nce)
+    gss = get_game_service_stub(nce)
+    print("Stubs received")
+    callback = PlayerCallbackImpl()
+    callback.set_username("Mark")
+    password = "pass123"
+    pss.login(retrieved_poa.resolve_servant_to_reference(callback), password)
