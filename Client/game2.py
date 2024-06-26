0d2fe057fd34ed340c1878c03f36bf6a1c934663
Gebreyl <2233944@slu.edu.ph>
2024-06-26 21:56:38 +0800

    initial game fix

--- Client/game2.py
+++ Client/game2.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import time
 import asyncio
 import json
@@ -6,6 +7,7 @@ import login
 from update_dispatcher import UpdateDispatcher
 from public import index as ORBConnection
 from Core import json_parser
+from concurrent.futures import ThreadPoolExecutor
 
 RESPONSE = {
     'response': None
@@ -20,24 +22,59 @@ class user_interface(UpdateDispatcher):
         self.response = ""
 
     def update(self, json_string: str):
-        print(json_string)
+        print('update', json_string)
         self.response = json_string
+        print("GAME UPDATE DATA")
+        state = json_parser.parse_status_state(json_string)
+
+        if state == "staging":
+            # room_id = json_parser.parse_room(json_string)
+            print("Round ", self.get_current_round(json_string))
+            letter_box = self.create_letter_box(json_string)
+
+            if letter_box:
+                for row in letter_box:
+                    print(row)
+
+        if state == "game_started":
+            print("GAME START")
+            word = input("\nEnter a word (5 Letters Or More)\n")
+            print("SUBMITTING A WORD...")
+            try:
+                self.submit_word(word, login.CURRENT_USER['username'], room_id)
+            except Exception as e:
+                print(e)
+            # calculated_points = 10
+            # print("\nSubmitted word:", word, "\nPoints:", calculated_points)
+            pass
+
+        if state == "game_done":
+            self.check_winner(json_string)
+            pass
+
+        if state == "invalid_word":
+            print("INVALID WORD, please try again")
+            pass
 
     def run(self):
         self.init_components()
-        while not self.response == 'game_ended':
+        while True:
             pass
-        print("GAME_ENDED")
+        # while not self.response == 'game_ended':
+        #     pass
+        # print("GAME_ENDED")
 
     def init_components(self):
         global RESPONSE
         try:
-            self.game_room_id = json_parser.parse_status_state(RESPONSE['response'])
+            self.game_room_id = json_parser.parse_game_room(RESPONSE['response'])
             orb = login.orb
             nce = ORBConnection.get_nce(orb)
             game_service_stub = ORBConnection.get_game_service_stub(nce)
-            print("Hanshake in progress")
+            print("Handshake in progress")
+            # with ThreadPoolExecutor() as executor:
             game_service_stub.readyHandshake(os.environ['username'], self.game_room_id)
+            #     executor.submit(game_service_stub.readyHandshake(os.environ['username'], self.game_room_id))
             print("Handshake success")
             print("5 second Countdown")
             time.sleep(5)
@@ -45,6 +82,84 @@ class user_interface(UpdateDispatcher):
             print("Player Sent ready")
         except Exception as e:
             print(e)
+
+    # def update_data(self, json_string):
+    #     print("GAME UPDATE DATA")
+    #     state = json_parser.parse_status_state(json_string)
+    #     room_id = json_parser.parse_room(json_string)
+    #
+    #     if state == "staging":
+    #         print("Round ", self.get_current_round(json_string))
+    #         letter_box = self.create_letter_box(json_string)
+    #         if letter_box:
+    #             for row in letter_box:
+    #                 print(row)
+    #
+    #         print("5 second Countdown")
+    #         time.sleep(5)
+    #         orb = login.orb
+    #         nce = ORBConnection.get_nce(orb)
+    #         game_service_stub = ORBConnection.get_game_service_stub(nce)
+    #         # Perform playerReady in a separate thread
+    #         with ThreadPoolExecutor() as executor:
+    #             executor.submit(game_service_stub.playerReady(os.environ['username'], self.game_room_id))
+    #         # print("5 second Countdown")
+    #         # time.sleep(5)
+    #         # orb = login.orb
+    #         # nce = ORBConnection.get_nce(orb)
+    #         # game_service_stub = ORBConnection.get_game_service_stub(nce)
+    #         #
+    #         # pool = ThreadPoolExecutor(max_workers=2)
+    #         # print("SUBMITTING THREAD")
+    #         # pool.submit(game_service_stub.playerReady(os.environ['username'], self.game_room_id))
+    #         # print("PLAYER READY")
+    #         # Create a ThreadPoolExecutor with 5 threads
+    #         # with ThreadPoolExecutor(max_workers=5) as executor:
+    #         #     executor.submit(game_service_stub.playerReady(os.environ['username'], self.game_room_id))
+    #
+    #     if state == "game_started":
+    #         print("GAME START")
+    #         word = input("\nEnter a word (5 Letters Or More)\n")
+    #         print("SUBMITTING A WORD...")
+    #         try:
+    #             self.submit_word(word, login.CURRENT_USER['username'], room_id)
+    #         except Exception as e:
+    #             print(e)
+    #         # calculated_points = 10
+    #         # print("\nSubmitted word:", word, "\nPoints:", calculated_points)
+    #         pass
+    #
+    #     if state == "game_done":
+    #         self.check_winner(json_string)
+    #         pass
+    #
+    #     if state == "invalid_word":
+    #         print("INVALID WORD, please try again")
+    #         pass
+    #
+    #     pass
+
+    def get_current_round(self, json_string):
+        try:
+            data = json.loads(json_string)
+            return data.get("current_round", None)
+        except json.JSONDecodeError as e:
+            print("Error parsing JSON:", e)
+            return None
+        pass
+
+    def create_letter_box(self, json_string):
+        try:
+            data = json.loads(json_string)
+            char_matrix = data.get("char_matrix", None)
+            if not char_matrix:
+                print("Character matrix not found in JSON.")
+                return None
+            return char_matrix
+        except json.JSONDecodeError as e:
+            print("Error parsing JSON:", e)
+            return None
+        pass
 
 
 class loader():
@@ -63,7 +178,7 @@ class loader():
             return False
             pass
         else:
-            print(RESPONSE['response'])
+            print('findmatch', RESPONSE['response'])
             return True
 
     def parse_match_making(self, json_string):
